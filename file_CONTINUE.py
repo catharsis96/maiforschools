c54b0b3b9650a5c06ec008c77cd4a0cb1699b64c
catharsis96 <snickerssuper@outlook.com>
2017-02-01 18:59:16 +0000

    Python

---
+++ file_CONTINUE.py
@@ -0,0 +1,14 @@
+#for i in range(5):
+ #   if i == 3:
+  #      continue   # skip the rest of the code inside loop for current i value
+   # print(i)
+
+for x in range(10):
+    if x % 2 == 0:   # Check if x is even:
+        continue   # skip print(x) for this loop
+    print(x)
+
+#for x in range(10):
+#    if x in range(2,10,2) :   # Check if x is even:
+#        continue   # skip print(x) for this loop
+#    print(x)
---
+++ file_IF.py
@@ -0,0 +1,10 @@
+#tasks = ['task1', 'task2']    # create new list
+#tasks = ['task1']    # create new list
+tasks = []    # create new list
+
+if len(tasks)==2:
+    print("Dva")
+elif len(tasks)==1:
+    print('Odin')
+else:
+    print('Null!')
